64ac98a595a92ef25f8b928ae80616d065a093a2
tienyiah <tienyiah@uci.edu>
2021-09-13 16:43:20 -0700

    Ready to run

---
+++ formal_project/02_derive_qflx_direct/config.jl
@@ -0,0 +1,76 @@
+using NCDatasets
+
+project_root_dir = @__DIR__
+data_dir = joinpath(project_root_dir, "data")
+domain_dir = joinpath(project_root_dir, "CESM_domains")
+casename = "Sandbox"
+
+config = Dict{Any, Any}(
+
+    "DRIVER" => Dict(
+        "casename"           => casename,
+        "caseroot"           => joinpath(project_root_dir, casename, "caseroot"),
+        "caserun"            => joinpath(project_root_dir, casename, "caserun"),
+        "archive_root"       => joinpath(project_root_dir, casename, "archive"),
+    ),
+
+    "MODEL_MISC" => Dict(
+        "timetype"               => "DateTimeNoLeap",
+        "init_file"              => joinpath(data_dir, "init_ocn.jld2"),
+        "rpointer_file"          => "rpointer.iom",
+        "daily_record"           => [],
+        "monthly_record"         => ["{ESSENTIAL}", "QFLX_TEMP", "QFLX_SALT"],
+        "enable_archive"         => true,
+    ),
+
+    "MODEL_CORE" => Dict(
+
+        "domain_file"                  => joinpath(domain_dir, "domain.ocn.gx3v7.120323.nc"),
+        "topo_file"                    => joinpath(data_dir, "Nz_bot.nc"),
+        "cdata_file"                   => joinpath(data_dir, "forcing.g37.nc"),
+
+        "cdata_beg_time"               => "0001-01-01 00:00:00",
+        "cdata_end_time"               => "0002-01-01 00:00:00",
+        "cdata_align_time"             => "0001-01-01 00:00:00",
+
+        "z_w"                          => nothing,
+
+        "substeps"                     => 8,
+        "MLD_scheme"                   => "datastream",
+        "Qflx"                         => "on",
+        "Qflx_finding"                 => "off",
+        "convective_adjustment"        => "on",
+        "advection_scheme"             => "ekman_AGA2020",
+
+        "weak_restoring"               => "on",
+        "τwk_TEMP"                     => 86400.0 * 365 * 1000,
+        "τwk_SALT"                     => 86400.0 * 365 * 1000,
+
+
+        "τ_frz"                        => 3600.0,
+        "Ekman_layers"                 => 5,
+        "Returnflow_layers"            => 25,
+    
+        "transform_vector_field"       => true,
+    ),
+
+)
+
+Dataset("coord.nc", "r") do ds
+
+    z_w_top = nomissing(ds["z_w_top"][:], NaN)
+    z_w_bot = nomissing(ds["z_w_bot"][:], NaN)
+
+    z_w      = zeros(Float64, length(z_w_top)+1)
+    z_w[1:end-1]   = z_w_top
+    z_w[end] = z_w_bot[end]
+
+    global config["MODEL_CORE"]["z_w"]  = z_w
+end
+
+
+using TOML
+
+open("config_verify.toml", "w") do io
+    TOML.print(io, config)
+end
