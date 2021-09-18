28d10bb98431276098083d562b10dbe8d6828d5b
tienyiah <tienyiah@uci.edu>
2021-09-17 19:41:21 -0700

    fix bug

--- src/models/EMOM/updateDatastream.jl
+++ src/models/EMOM/updateDatastream.jl
@@ -14,9 +14,11 @@ function updateDatastream!(
         if cfg["MLD_scheme"] == "datastream"
             mb.fi.HMXL .= datastream["HMXL"]
         end
-
-        mb.fi._QFLXX_[:, 1] .= reshape(datastream["QFLXT"], :)
-        mb.fi._QFLXX_[:, 2] .= reshape(datastream["QFLXS"], :)
+        
+        if cfg["Qflx"] == "on"
+            mb.fi._QFLXX_[:, 1] .= reshape(datastream["QFLXT"], :)
+            mb.fi._QFLXX_[:, 2] .= reshape(datastream["QFLXS"], :)
+        end
 
     end
 
