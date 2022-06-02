611344f842e6a640d564fcb6b6dd5460cc2875d1
xtt <tienyiah@uci.edu>
2022-06-02 01:36:55 -0600

    edit

--- tutorial/02_derive_reference_profile/lib/main_concat_and_convert_units.jl
+++ tutorial/02_derive_reference_profile/lib/main_concat_and_convert_units.jl
@@ -2,12 +2,6 @@ using Formatting
 using ArgParse
 using DataStructures, JSON
 
-println("""
-This program generates the TEMP SALT daily mean and five-day mean profiles from a referenced
-POP2 output. These output are meant to used for Q-flux finding.
-""")
-
-
 s = ArgParseSettings()
 @add_arg_table s begin
 
@@ -37,17 +31,20 @@ s = ArgParseSettings()
         arg_type = Int64
         default = 33
 
-
 end
 
 parsed = DataStructures.OrderedDict(parse_args(ARGS, s))
 
 JSON.print(parsed, 4)
 
+
+
+
 function runOneCmd(cmd)
     println(">> ", string(cmd))
     run(cmd)
 end
+
 
 function pleaseRun(cmd)
     if isa(cmd, Array)
@@ -69,13 +66,14 @@ println("End   year: $(end_year)")
 layers = parsed["layers"]
 in_dir = parsed["hist-dir"]
 
+
+println("Begin year: $(beg_year)")
+println("End   year: $(end_year)")
+
 coord = "z_t,z_w,z_w_top,z_w_bot,TAREA"
-varnames = "HMXL,TEMP,SALT"
+varnames = "UVEL,VVEL"
 
 fileformats = Dict(
-    "TEMP" => "{}.pop.h.nday1.{}-{}-01.nc",
-    "SALT" => "{}.pop.h.nday1.{}-{}-01.nc",
-    "HMXL" => "{}.pop.h.nday1.{}-{}-01.nc",
     "UVEL" => "{}.pop.h.{}-{}.nc",
     "VVEL" => "{}.pop.h.{}-{}.nc",
 )
@@ -83,18 +81,21 @@ fileformats = Dict(
 year_rng      = format( "{:04d}-{:04d}",  beg_year, end_year )
 year_rng_eval = format( "{{{:04d}..{:04d}}}",  beg_year, end_year )
 
-ref_file = joinpath(in_dir, format(fileformats["TEMP"], casename, format("{:04d}", beg_year), "01"))
+ref_file = joinpath(in_dir, format(fileformats["UVEL"], casename, format("{:04d}", beg_year), "01"))
 
-out_dir  = parsed["output-dir"]
+out_dir="./output_$(year_rng)_layers$(layers)"
 coord_file="$(out_dir)/coord.nc"
 time_file="$(out_dir)/time.nc"
+
 
 if isdir(out_dir)
     throw(ErrorException("ERROR: directory $(out_dir) already exists."))
 end
 
+#pleaseRun(`rm -rf $(out_dir)`)
+
 pleaseRun(`mkdir -p $(out_dir)`)
-pleaseRun(`julia $(@__DIR__)/make_daily_time.jl --output $(time_file) --years 1`)
+pleaseRun(`julia make_monthly_time.jl --output $(time_file) --years $( end_year - beg_year + 1 )`)
 
 println("Output directory: $(out_dir)")
 
@@ -104,7 +105,7 @@ pleaseRun(`ncap2 -O -s 'z_t=-z_t/100.0;z_w_top=-z_w_top/100.0;z_w_bot=-z_w_bot/1
 output_files = Dict()
 
 for varname in split(varnames, ",")
-
+    
     output_file = "$(out_dir)/$(varname).nc"
     output_files[varname] = output_file
     
@@ -112,28 +113,46 @@ for varname in split(varnames, ",")
 
     tmp_dir = "tmp_$(varname)" 
     mkpath(tmp_dir)
-    
+ 
     fileformat = fileformats[varname]
     filenames = []
 
-    for m=1:12
-        
-        m_str = format("{:02d}", m)
-        tmp_file = "$(tmp_dir)/$(varname)_$(m_str).nc"
-        files = format(fileformat, casename, year_rng_eval, m_str)
-        files = "$in_dir/$files"
-        pleaseRun(`bash -c "ncea -O -F -d z_t,1,$(layers) -d z_w_top,1,$(layers) -d z_w_bot,1,$(layers) -v $varname $files $tmp_file"`)
 
-        push!(filenames, tmp_file)
+    if varname != "HMXL"
+        pleaseRun(`bash -c "ncrcat -O -F -v $(varname) -d z_t,1,$(layers) -d z_w_top,1,$(layers) -d z_w_bot,1,$(layers) $(in_dir)/$(casename).pop.h.{$(year_rng_eval)}-{01..12}.nc $(output_file)"`)
+    else
+
+        # HMXL was output in daily. So we have to average it to monthly
+
+        mkpath("tmp_HMXL")
+        for y=beg_year:end_year, m=1:12
+            timestr = format("{:04d}-{:02d}", y, m)
+            pleaseRun(`ncra -O -F -v HMXL $(in_dir)/$(casename).pop.h.nday1.$(timestr)-01.nc tmp_HMXL/HMXL_$(timestr).nc`)
+        end
+        pleaseRun(`bash -c "ncrcat -O -F -v HMXL tmp_HMXL/HMXL_{$(year_rng_eval)}-{01..12}.nc $(output_file)"`)
+
+        
+
     end
 
-    pleaseRun(`bash -c "ncrcat -O -F $(join(filenames, " ")) $(output_file)"`)
-    
-    rm(tmp_dir, recursive=true, force=true)
 end
 
-println("Converting units...")
+println("Converting forcing for IOM...")
+
+for varname in ["VSFLX", "HMXL", "SWFLX", "NSWFLX"]
+    output_files[varname] = "$(out_dir)/$(varname).nc"
+end
+
+pleaseRun(`ncap2 -O -v -s 'VSFLX=SFWF;'      $(output_files["SFWF"]) $(output_files["VSFLX"])`)
 pleaseRun(`ncap2 -O -v -s 'HMXL=HMXL/100.0;' $(output_files["HMXL"]) $(output_files["HMXL"])`)
+pleaseRun(`ncap2 -O -v -s 'TAUX=TAUX/10.0;'  $(output_files["TAUX"]) $(output_files["TAUX"])`)
+pleaseRun(`ncap2 -O -v -s 'TAUY=TAUY/10.0;'  $(output_files["TAUY"]) $(output_files["TAUY"])`)
+pleaseRun(`ncap2 -O -v -s 'SWFLX=-SHF_QSW;'  $(output_files["SHF_QSW"]) $(output_files["SWFLX"])`)
+
+pleaseRun(`ncks  -O    -v SHF                      $(output_files["SHF"])     $(output_files["NSWFLX"])`)
+pleaseRun(`ncks     -A -v SHF_QSW                  $(output_files["SHF_QSW"]) $(output_files["NSWFLX"])`)
+pleaseRun(`ncap2 -O -v -s 'NSWFLX=-(SHF-SHF_QSW);' $(output_files["NSWFLX"])  $(output_files["NSWFLX"])`)
+
 
 for (_, output_file) in output_files
     println(coord_file, "; ", output_file)
@@ -141,33 +160,34 @@ for (_, output_file) in output_files
     pleaseRun(`ncks -A -v time,time_bound $time_file $output_file`)
 end
 
-# Get surface U and V
-for (varname, old_output_file) in output_files
-    if varname in ["UVEL", "VVEL"]
 
-        new_varname = Dict(
-            "UVEL" => "USFC",
-            "VVEL" => "VSFC",
-        )[varname]
+println("Doing monthly mean:")
 
-        new_output_file = "$(out_dir)/$(new_varname).nc"
+out_dir_monthly = "./$(out_dir)/monthly"
+tmp_dir = "$(out_dir_monthly)/tmp"
+mkpath(tmp_dir)
 
-        pleaseRun(`bash -c "ncks -O -F -d z_t,1,1 -v $(varname) $(output_file) $(new_output_file)"`)
-        pleaseRun(`bash -c "ncrename -v $(varname),$(new_varname) $(new_output_file)"`)
-        output_files[new_varname] = new_output_file
+a_year_time_file = "$(tmp_dir)/time.nc"
+
+pleaseRun(`julia make_monthly_time.jl --output $(a_year_time_file) --years 1`)
+    
+output_files_monthly = Dict()
+for varname in keys(output_files)
+
+    println("Doing variable $(varname)")
+
+    output_files_monthly[varname] = "$(out_dir_monthly)/$(varname)_monthly.nc"
+
+    for m = 1:12
+        pleaseRun(`ncra -O -F -d time,$(m),,12 $(output_files[varname]) $tmp_dir/$(varname)_$(format("{:02d}", m)).nc`)
     end
+    
+    pleaseRun(`bash -c "ncrcat -O -F $tmp_dir/$(varname)_{01..12}.nc $(output_files_monthly[varname])"`)
+    pleaseRun(`ncks -A -v time,time_bound $a_year_time_file $(output_files_monthly[varname])`)
+
 end
 
-varnames_daily = ["HMXL", "TEMP", "SALT"]
-println("Doing 5 day mean of variables: ", join(varnames_daily, ","))
-fivedays_mean_dir = "$(out_dir)/fivedays_mean"
-mkpath(fivedays_mean_dir)
-for varname in varnames_5days
-    daily_output_file = output_files[varname]
-    fivedays_mean_output_file = "$(fivedays_mean_dir)/$(varname).nc"
-    println("$daily_output_file => $fivedays_mean_output_file")
-    pleaseRun(`ncra --mro -d time,0,,5,5 $daily_output_file $fivedays_mean_output_file`)
-end
+rm(tmp_dir, recursive=true, force=true)
 
 
 
